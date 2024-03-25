dc37e7ce74c77638fdb9393759d55b4767cbe70f
Daniel <danpoco@gmail.com>
2024-03-25 01:09:08 -0300

    Criado o arquivo database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function getStatus(req, res) {
+import database from "../../../../infra/database.js";
+
+async function getStatus(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({
     valor: "Os alunos do curso.dev são pessoas acima da média",
   });
